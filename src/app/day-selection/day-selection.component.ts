98b3c6f059dc0399d3a2574e9168d9ab357ce872
maialen <maialenmunoa@gmail.com>
2024-05-07 11:36:08 +0200

    add day selection component

---
+++ src/app/day-selection/day-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './day-selection.component.html',
+  styleUrl: './day-selection.component.scss'
+})
+export class DaySelectionComponent {
+
+}
