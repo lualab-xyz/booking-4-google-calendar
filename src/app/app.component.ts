20f1de4fff3100968c6431c041408bb4d5be206a
maialen <maialenmunoa@gmail.com>
2024-05-07 12:08:08 +0200

    add components to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { DaySelectionComponent } from './day-selection/day-selection.component';
+import { CalendarComponent } from './calendar/calendar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DaySelectionComponent],
+  imports: [RouterOutlet, DaySelectionComponent, CalendarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'booking-app';
+
+  // Método que se llama cuando se emite el evento daySelected
+  onDaySelected(day: string) {
+    console.log(day); // Aquí puedes manejar el día seleccionado
+  }
 }
